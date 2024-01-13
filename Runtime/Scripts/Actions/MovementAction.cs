b59682a560dfa098e5b3e7abb795b42157789ff6
Talal <>
2024-01-13 15:05:33 +0500

    reading movement input from ui actions also

--- Runtime/Scripts/Actions/MovementAction.cs
+++ Runtime/Scripts/Actions/MovementAction.cs
@@ -7,29 +7,57 @@ namespace Stax3.Plugins.InputSystem
     [CreateAssetMenu(fileName = "Movement Action", menuName = "Stax3/Plugins/Actions/Movement Action")]
     public class MovementAction : Input_Action
     {
+        private Vector2 m_newInputData = Vector2.zero;
         private Vector2 m_vector2Input = Vector2.zero;
         private bool invokeOnceOnZero;
         protected override void SubscribeEvents()
         {
+            action.performed += (context) =>
+            {
+                m_newInputData = context.ReadValue<Vector2>();
+                callback?.Invoke(new CallbackData { vector2Input = m_newInputData });
+            };
+            action.canceled += (context) =>
+            {
+                m_newInputData = Vector2.zero;
+                callback?.Invoke(new CallbackData { vector2Input = m_newInputData });
+            };
             InputManager.Instance.monoUpdate.update += CheckifInputSuccess;
         }
         public override void UnSubscribeEvents()
         {
+            action.performed -= (context) =>
+            {
+                m_newInputData = context.ReadValue<Vector2>();
+                callback?.Invoke(new CallbackData { vector2Input = m_newInputData });
+            };
+            action.canceled -= (context) =>
+            {
+                m_newInputData = Vector2.zero;
+                callback?.Invoke(new CallbackData { vector2Input = m_newInputData });
+            };
             InputManager.Instance.monoUpdate.update -= CheckifInputSuccess;
         }
         private void CheckifInputSuccess()
         {
-            m_vector2Input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            if (m_vector2Input.sqrMagnitude > 0)
+            if (m_newInputData.sqrMagnitude == 0)
             {
-                callback?.Invoke(new CallbackData { vector2Input = m_vector2Input });
-                invokeOnceOnZero = true;
+                m_vector2Input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+                if (m_vector2Input.sqrMagnitude > 0)
+                {
+                    callback?.Invoke(new CallbackData { vector2Input = m_vector2Input });
+                    invokeOnceOnZero = true;
+                }
+                else if (invokeOnceOnZero)
+                {
+                    callback?.Invoke(new CallbackData { vector2Input = m_vector2Input });
+                    invokeOnceOnZero = false;
+                }
             }
-            else if (invokeOnceOnZero)
-            {
-                callback?.Invoke(new CallbackData { vector2Input = m_vector2Input });
-                invokeOnceOnZero = false;
-            }
+        }
+        private void OnDisable()
+        {
+            m_newInputData = m_vector2Input = Vector2.zero;
         }
     }
 }
